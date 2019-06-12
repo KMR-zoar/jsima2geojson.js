996d790307d5249fed718a80bf3276b36dad76cd
Zoar <zoar@k-side.net>
2019-06-12 14:53:25 +0900

    ライン地物の作成機能を追加

--- test/jpgisObject.Spec.js
+++ test/jpgisObject.Spec.js
@@ -14,18 +14,21 @@ describe('JPGISオブジェクトの変換', () => {
     simaObj['jps:GM_Point'],
     epsg
   )
-  
+
+  const contentPointObj = require('./data/pointObject.json')
+  const contentLineObj = require('./data/lineObject.json')
+
   it('ポイントオブジェクトの作成', () => {
-    const content = JSON.stringify({
-      pnt000001: [139.8333333333333, 36],
-      pnt000002: [139.8333333333333, 36.000090132390916],
-      pnt000003: [139.8334442539458, 36.000090132339636],
-      pnt000004: [139.83344425381958, 35.99999999994872],
-      pnt000005: [139.8333333333333, 35.99990986760772],
-      pnt000006: [139.83344425369336, 35.99990986755644],
-      pnt000007: [139.83338879360798, 36.00004506618281],
-      pnt000008: [139.83338879354488, 35.99995493379121]
-    })
+    const content = JSON.stringify(contentPointObj)
     assert.equal(content, JSON.stringify(pointObject))
+  })
+
+  const lineObject = jpgisObject.gmCurve2LineStringObject(
+    simaObj['jps:GM_Curve'],
+    pointObject
+  )
+  it('ライン地物の作成', () => {
+    const content = JSON.stringify(contentLineObj)
+    assert.equal(content, JSON.stringify(lineObject))
   })
 })
