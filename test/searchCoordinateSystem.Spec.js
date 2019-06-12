5f9ae670af5c89e5773ab9f53fd03e688a5bdd35
Zoar <zoar@k-side.net>
2019-06-12 14:15:07 +0900

    XML→JSONオブジェクトから測理系等を取り出す

---
+++ test/searchCoordinateSystem.Spec.js
@@ -0,0 +1,15 @@
+const assert = require('assert')
+const searchCoordSys = require('../lib/searchCoordinateSystem')
+
+const xmlObj = require('./dummy.json')
+
+describe('XMLを変換したJSONから測地系を取得する', () => {
+  it('ダミーファイルから取得', () => {
+    const content = JSON.stringify({
+      coordinateSystem: '9',
+      crs: '2'
+    })
+    const result = searchCoordSys(xmlObj)
+    assert.equal(content, JSON.stringify(result))
+  })
+})
