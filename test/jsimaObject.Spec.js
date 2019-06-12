6481752bbed4511e106d2fe1e6b76a21b45e718f
Zoar <zoar@k-side.net>
2019-06-12 15:35:24 +0900

    地番名付きポリゴンオブジェクトの作成機能を追加

--- test/jsimaObject.Spec.js
+++ test/jsimaObject.Spec.js
@@ -1,8 +1,9 @@
 const assert = require('assert')
-const searchCoordSys = require('../lib/jsimaObject').searchCoordinateSystem
+const jsima = require('../lib/jsimaObject')
 const getEPSG = require('../lib/coordinateSystem')
 
 const xmlObj = require('./data/dummy.json')
+const polygonObject = require('./data/polygonObject.json')
 
 describe('XMLを変換したJSONから測地系を取得する', () => {
   it('ダミーファイルから取得', () => {
@@ -10,13 +11,24 @@ describe('XMLを変換したJSONから測地系を取得する', () => {
       coordinateSystem: '9',
       crs: '2'
     })
-    const result = searchCoordSys(xmlObj)
+    const result = jsima.searchCoordinateSystem(xmlObj)
     assert.equal(content, JSON.stringify(result))
   })
   it('ダミーファイルからEPSGコードを取得', () => {
     const content = '6677'
-    const coord = searchCoordSys(xmlObj)
+    const coord = jsima.searchCoordinateSystem(xmlObj)
     const result = getEPSG[coord.crs][coord.coordinateSystem]
     assert.equal(content, result)
+  })
+
+  const simaObj = xmlObj['jsima:GI']['jsima:dataset'][0]['jsima:object'][0]
+  const namedPolygonObject = jsima.chibanIntoproperties(
+    simaObj['jsima:Kakuchi'],
+    polygonObject
+  )
+  it('名前付きポリゴンの作成', () => {
+    const contentObject = require('./data/namedObject.json')
+    const content = JSON.stringify(contentObject)
+    assert.equal(content, JSON.stringify(namedPolygonObject))
   })
 })
