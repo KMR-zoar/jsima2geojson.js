8e81c6f59f3c28e0607868a3d7058be635a60a77
Zoar <zoar@k-side.net>
2019-06-12 14:25:33 +0900

    測地系のテストを追加

--- test/jsimaObject.Spec.js
+++ test/jsimaObject.Spec.js
@@ -1,5 +1,6 @@
 const assert = require('assert')
 const searchCoordSys = require('../lib/jsimaObject').searchCoordinateSystem
+const getEPSG = require('../lib/coordinateSystem')
 
 const xmlObj = require('./dummy.json')
 
@@ -11,5 +12,11 @@ describe('XMLを変換したJSONから測地系を取得する', () => {
     })
     const result = searchCoordSys(xmlObj)
     assert.equal(content, JSON.stringify(result))
+  })
+  it('ダミーファイルからEPSGコードを取得', () => {
+    const content = '6677'
+    const coord = searchCoordSys(xmlObj)
+    const result = getEPSG[coord.crs][coord.coordinateSystem]
+    assert.equal(content, result)
   })
 })
